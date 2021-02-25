cfe3aa51bc79f945404040afc9531fa5cdeae46d
aimee-liang <aimeeeliang@gmail.com>
2021-02-25 17:19:33 -0500

    401 Error

--- basic-ecommerce-store/src/server.js
+++ basic-ecommerce-store/src/server.js
@@ -1,4 +1,5 @@
-const stripe = require('stripe')('sk_test_51IIKpuG5DSaF9r7jz8YAOzk3E7GrtTXprJceOF1tAf33ISoGWokDTPsfuiTcjktT5dLk7SHs79fpVt5IDcxBwVPn00hclj6ir2');
+const Stripe = require('pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8')
+const stripe = require('pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8')('sk_test_51IIKpuG5DSaF9r7jz8YAOzk3E7GrtTXprJceOF1tAf33ISoGWokDTPsfuiTcjktT5dLk7SHs79fpVt5IDcxBwVPn00hclj6ir2');
 
 stripe.customers.create({
   email: 'customer@example.com',
