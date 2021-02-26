72a15843dde29d7208c4b7c5ae7e0000afcd2120
aimee-liang <aimeeeliang@gmail.com>
2021-02-26 16:09:10 -0500

    Removed useState and trying to fetch from CardInfo

--- basic-ecommerce-store/src/Cart/CardInfo.js
+++ basic-ecommerce-store/src/Cart/CardInfo.js
@@ -2,18 +2,7 @@ import React, {useState} from 'react'
 
 const CardInfo = () => {
 
-    const [payment, setPayment] = useState([])
-    const stripe = Stripe("pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8")
-
-    React.useEffect(function effectFunction(){
-        async function fetchStripe() {
-            const response = await fetch("https://api.stripe.com")
-            const json = await response.json()
-            setPayment(json.data)
-        }
-        fetchStripe()
-    }, [])
-
+    // const [payment, setPayment] = useState([])
     
     return(
         <>
