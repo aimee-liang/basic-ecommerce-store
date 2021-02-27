f5f8aea484de022940e843e1cf23728d78978283
aimee-liang <aimeeeliang@gmail.com>
2021-02-27 18:39:54 -0500

    Considering moving to Snipcart or Paypal

--- basic-ecommerce-store/src/Cart/CardInfo.js
+++ basic-ecommerce-store/src/Cart/CardInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CardInfo = () => {
 
     // const [payment, setPayment] = useState([])
-    const stripe = Stripe("pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8")
+    // const stripe = Stripe("pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8")
     // const elements = stripe.elements()
 
     return(
