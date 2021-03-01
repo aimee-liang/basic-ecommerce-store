86e16dad111e9357a4211ac37efbc099e6b52cb6
aimee-liang <aimeeeliang@gmail.com>
2021-02-28 20:31:10 -0500

    Backend needed...

--- basic-ecommerce-store/src/Cart/CardInfo.js
+++ basic-ecommerce-store/src/Cart/CardInfo.js
@@ -3,10 +3,9 @@ import CheckoutForm from "./CheckoutForm"
 import {Elements} from "@stripe/react-stripe-js"
 import {loadStripe} from "@stripe/stripe-js"
 
+const stripePromise = loadStripe("pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8")
+
 const CardInfo = () => {
-
-    const stripePromise = loadStripe("pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8")
-
     return(
         <div className="card-info-terminal">
             <Elements stripe={stripePromise}>
