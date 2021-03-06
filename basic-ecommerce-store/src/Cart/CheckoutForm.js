42209c4138e3db183d22798482c4b98010bc5415
aimee-liang <aimeeeliang@gmail.com>
2021-03-06 17:12:45 -0500

    Passed Stripe Key and creating handleToken()

--- basic-ecommerce-store/src/Cart/CheckoutForm.js
+++ basic-ecommerce-store/src/Cart/CheckoutForm.js
@@ -1,9 +1,14 @@
 import React from "react"
 import StripeCheckout from 'react-stripe-checkout'
 
+
+const handleToken = () => {
+
+}
+
 const CheckOutForm = () => {
     return (
-        <StripeCheckout/>
+        <StripeCheckout stripeKey="pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8" token={}/>
     )
 }
 
