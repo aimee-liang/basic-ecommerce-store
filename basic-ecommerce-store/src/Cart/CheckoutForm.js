df4b0758cd17badaf80144d0e68b570f66e4dc98
aimee-liang <aimeeeliang@gmail.com>
2021-03-06 17:14:52 -0500

    Trying to work with billing, shipping, and amount

--- basic-ecommerce-store/src/Cart/CheckoutForm.js
+++ basic-ecommerce-store/src/Cart/CheckoutForm.js
@@ -1,14 +1,17 @@
 import React from "react"
 import StripeCheckout from 'react-stripe-checkout'
+import Item from "../Items/Item"
 
 
-const handleToken = () => {
+const handleToken = (token, addresses) => {
 
 }
 
 const CheckOutForm = () => {
     return (
-        <StripeCheckout stripeKey="pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8" token={}/>
+        <StripeCheckout stripeKey="pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8" token={handleToken}
+            billingAddress shippingAddress amount={item.price}
+        />
     )
 }
 
