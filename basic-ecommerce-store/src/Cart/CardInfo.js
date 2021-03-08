78fc7b194232a5afb16c701ef18a34d8a9036b75
aimee-liang <aimeeeliang@gmail.com>
2021-03-08 15:27:31 -0500

    Removed previous stripe.js imports from CardInfo and fixed disclaimer

--- basic-ecommerce-store/src/Cart/CardInfo.js
+++ basic-ecommerce-store/src/Cart/CardInfo.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import CheckoutForm from "./CheckoutForm"
-import {Elements} from "@stripe/react-stripe-js"
-import {loadStripe} from "@stripe/stripe-js"
 
-const stripePromise = loadStripe("pk_test_51IIKpuG5DSaF9r7j6sxZCK0tppgqaztwXBxuHnJLx2Y8RvgWzpciJeLFT0P1wrd4fZZGH5bMAsp0JdvzTBrpuCqr00m23vKKZ8")
 
 const CardInfo = () => {
     return(
         <div className="card-info-terminal">
-            <Elements stripe={stripePromise}>
-                <CheckoutForm/>
-            </Elements>
+            <CheckoutForm/>
         </div>
     )
 }
