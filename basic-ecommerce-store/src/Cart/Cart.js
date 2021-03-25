4136ff652097c47653ddb376dfbd0c711bbd3c21
aimee-liang <aimeeeliang@gmail.com>
2021-03-25 18:44:27 -0400

    Cart.js breaks everything :(

--- basic-ecommerce-store/src/Cart/Cart.js
+++ basic-ecommerce-store/src/Cart/Cart.js
@@ -7,33 +7,33 @@ const Cart = (props) => {
     const [cart, setCart] = useState([])
     const [key, setKey] = useState()
 
-    const calculateTotal = ([...items]) => {
-        return items["price"].reduce((sum, value) => {return sum + value}, 0)
-    }
+    // const calculateTotal = ([...items]) => {
+    //     return items["price"].reduce((sum, value) => {return sum + value}, 0)
+    // }
 
     const clearCart = () => {
         setCart([cart.length === 0])
     }
 
     /* take in items and key of item that has been clicked on */
-    const increaseQuantity = (items, key) => {
-        setKey(key)
-        items.filter((item) => {
-            if (item.key === key){
-                return parseInt(item["price"] * 2) && parseInt(item["quantity"] + 1)
-            } 
-        })
-    }
+    // const increaseQuantity = (items, key) => {
+    //     setKey(key)
+    //     items.filter((item) => {
+    //         if (item.key === key){
+    //             return parseInt(item["price"] * 2) && parseInt(item["quantity"] + 1)
+    //         } 
+    //     })
+    // }
 
-    const decreaseQuantity = (items, key) => {
-        setKey(key)
-        items.filter((item) => {
-            if (item.key === key){
-                let amt = item["price"]
-                return parseInt(item["price"] -= amt) && parseInt(item["quantity"] - 1)
-            }
-        })
-    }
+    // const decreaseQuantity = (items, key) => {
+    //     setKey(key)
+    //     items.filter((item) => {
+    //         if (item.key === key){
+    //             let amt = item["price"]
+    //             return parseInt(item["price"] -= amt) && parseInt(item["quantity"] - 1)
+    //         }
+    //     })
+    // }
 
     // const renderItems = props.total.map((item, index) => <CartItem key={index} item={item} />)
 
@@ -44,11 +44,12 @@ const Cart = (props) => {
                     <>
                     <h2>Your Cart</h2>
                         {/* {renderItems} */}
-                        <h2>Total: ${calculateTotal(cart)}</h2>
+                        <h2>Total: $</h2>
+                        {/* <h2>Total: ${calculateTotal(cart)} </h2> */}
                         <CardInfo/>
                         <button onClick={clearCart}>Clear cart</button>
-                        <button onClick={increaseQuantity(cart)}>+</button>
-                        <button onClick={decreaseQuantity(cart)}>-</button>
+                        {/* <button onClick={increaseQuantity(cart)}>+</button> */}
+                        {/* <button onClick={decreaseQuantity(cart)}>-</button> */}
                     </>
                 }
             </div>
