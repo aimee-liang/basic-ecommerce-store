fcd779c8e72892fd7f8f373f526c864edb8e8d94
aimee-liang <aimeeeliang@gmail.com>
2021-03-30 22:33:49 -0400

    Fixing bugs and logic at App level

--- basic-ecommerce-store/src/App.js
+++ basic-ecommerce-store/src/App.js
@@ -3,21 +3,23 @@ import React, {useState} from "react"
 import Logo from "./Logo"
 import Cart from "./Cart/Cart"
 import Stock from "./Items/Stock"
-// import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const App = () => {
 
   const [total, setTotal] = useState([]) 
-  // const [cartDisplay, setCartDisplay] = useState(false)
+  
+  const updateTotal = (item) => setTotal([...item]) /* item is an object in an array to show price and quant. */
+  const [cartDisplay, setCartDisplay] = useState(false)
 
-  const updateTotal = (item) => setTotal([...item])
 
   return (
     <>
       <Logo />
       <Stock updateTotal={updateTotal}/>
       <Cart cartTotal={[...total]}/>
-      {/* <ShoppingCartIcon onClick={() => setCartDisplay(true)} /> */}
+      <ShoppingCartIcon onClick={() => setCartDisplay(true)} />
+        {cartDisplay === true ? <Cart cartTotal={[...total]}/> : null } {/* does this work? */}
       <h4>Website design by <a href="https://aimeeliang.com">Aimee Liang</a></h4>
       <h5>Please note this is a demo app. Do not enter your actual payment information.</h5>
     </>
