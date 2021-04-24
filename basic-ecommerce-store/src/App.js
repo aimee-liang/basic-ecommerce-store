33a31708781a1077d8896cd075ddac28c3811d7c
aimee-liang <aimeeeliang@gmail.com>
2021-04-23 20:40:57 -0400

    Cleaning up App level logic

--- basic-ecommerce-store/src/App.js
+++ basic-ecommerce-store/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import Logo from "./Logo"
 import Cart from "./Cart/Cart"
 import Stock from "./Items/Stock"
@@ -8,19 +8,30 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 const App = () => {
 
   const [total, setTotal] = useState([]) 
+  const [inStock, setInStock] = useState([])
   
   const updateTotal = (item) => setTotal([...item]) /* item is an object in an array to show price and quant. */
   const [cartDisplay, setCartDisplay] = useState(false)
 
   /* fetch API */
+  useEffect(() => {
+    renderAPIData()
+  }, [])
+
+  const renderAPIData = () => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setInStock(data))
+      .catch(error => console.log(error))
+  }
 
   return (
     <>
       <Logo />
-      <Stock updateTotal={updateTotal}/>
-      <Cart cartTotal={[...total]}/>
-      <ShoppingCartIcon onClick={() => setCartDisplay(true)} />
-        {cartDisplay === true ? <Cart cartTotal={[...total]}/> : null } {/* does this work? */}
+      <Stock inStock={inStock}/>
+      {/* <Cart cartTotal={[...total]}/> */}
+      {/* <ShoppingCartIcon onClick={() => setCartDisplay(true)} /> */}
+        {/* {cartDisplay === true ? <Cart cartTotal={[...total]}/> : null } */}
       <h4>Website design by <a href="https://aimeeliang.com">Aimee Liang</a></h4>
       <h5>Please note this is a demo app. Do not enter your actual payment information.</h5>
     </>
