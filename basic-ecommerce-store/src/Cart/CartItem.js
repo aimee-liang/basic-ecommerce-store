5b695aea6c32671efb0294a211ad7504529abb19
aimee-liang <aimeeeliang@gmail.com>
2021-04-28 23:19:06 -0400

    Cleaning up CartItem.js

--- basic-ecommerce-store/src/Cart/CartItem.js
+++ basic-ecommerce-store/src/Cart/CartItem.js
@@ -11,26 +11,15 @@ const CartItem = (props) => {
     const localIncrease = () => {
         // props.increaseQuantity()
             /* some kind of confirmation or popup you've increased quantity? with timeout? */
-        console.log("hello")
     }
 
     const localDecrease = () => {
         // props.decreaseQuantity()
             /* some kind of confirmation or popup you've decreased quantity? with timeout? */
-        console.log("goodbye!")
     }
 
     return(
         <>
-            <div className="cart-item">
-                <img alt={props.item.name} src={props.item.image} />
-                <h3>{props.item.name}</h3>
-                <h4>{props.item.price}</h4>
-                <Button onClick={localRemove}>Remove From Cart</Button>
-                <Button variant="contained" color="primary" onClick={localIncrease}>+</Button>
-                <Button variant="contained" color="secondary" onClick={localDecrease}>-</Button>
-                <h3>{props.calculateTotal()}</h3>
-            </div>
         </>
     )
 }
